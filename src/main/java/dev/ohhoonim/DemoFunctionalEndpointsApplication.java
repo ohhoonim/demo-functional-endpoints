8e7002c2c871ce037bbcde71d45692e15d12641c
주형돈 <hdju@rntime.com>
2026-03-19 14:41:24 +0900

    init

---
+++ src/main/java/dev/ohhoonim/DemoFunctionalEndpointsApplication.java
@@ -0,0 +1,13 @@
+package dev.ohhoonim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoFunctionalEndpointsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoFunctionalEndpointsApplication.class, args);
+	}
+
+}
