a1ee9fa6529e91aad482eb4362d64b4a5d0b58b3
mathew <donnj95@gmail.com>
2026-03-21 14:50:18 +0900

    post controller

---
+++ src/main/java/dev/ohhoonim/post/PostController.java
@@ -0,0 +1,18 @@
+package dev.ohhoonim.post;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/posts")
+public class PostController {
+    
+    @GetMapping("{id}")
+    public String getMethodName(@PathVariable("id") String postId) {
+        return "posts : " + postId;
+    }
+    
+}
---
+++ src/test/java/dev/ohhoonim/post/PostControllerTest.java
@@ -0,0 +1,21 @@
+package dev.ohhoonim.post;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.assertj.MockMvcTester;
+
+@WebMvcTest(PostController.class)
+class PostControllerTest {
+    
+    @Autowired
+    MockMvcTester mockMvc;
+
+    @Test
+    void getPost() {
+        mockMvc.get().uri("/posts/{id}", "PRD-A11D2")
+            .contentType(MediaType.APPLICATION_JSON)
+            .assertThat().bodyText().isEqualTo("posts : PRD-A11D2");
+    }
+}
